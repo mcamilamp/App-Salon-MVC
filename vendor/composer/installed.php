0886096c4b193f565f7b931208afbeb9d74b2cd0
mcamilamp <camimerpa@gmail.com>
2024-12-03 11:09:44 -0500

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'maria-camila/app-salon_php_mvc_js_sass',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'eee88bd58f18dfe1091fbb4a4f9a47200b65802b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'maria-camila/app-salon_php_mvc_js_sass' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'eee88bd58f18dfe1091fbb4a4f9a47200b65802b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ views/auth/login.php
@@ -0,0 +1,16 @@
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia Sesión con tus datos</p>
+
+<form action="/" class="formulario" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="text" id="email" placeholder="Tu Email" name="email">
+    </div>
+
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Tu Password" name="password">
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesión">
+</form>
